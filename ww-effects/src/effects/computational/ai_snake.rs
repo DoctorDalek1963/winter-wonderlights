21a5c3e612794c92273d0207846f7a120a5e6890
DoctorDalek1963 <dyson.dyson@icloud.com>
2023-09-06 16:14:59 +0100

    Add pathfinding and auto-resetting

--- ww-effects/src/effects/computational/ai_snake.rs
+++ ww-effects/src/effects/computational/ai_snake.rs
@@ -89,8 +89,9 @@ mod config {
 #[cfg(feature = "effect-impls")]
 mod effect {
     use super::*;
+    use ordered_float::NotNan;
     use rand::Rng;
-    use std::collections::VecDeque;
+    use std::{collections::VecDeque, fmt};
     use ww_gift_coords::{GIFTCoords, COORDS};
 
     /// A coordinate in snake space.
@@ -98,14 +99,24 @@ mod effect {
     struct Coord(i8, i8, i8);
 
     impl Coord {
-        /// Check if this coordinate is adjacent to another one.
-        fn is_adjacent(self, other: Self) -> bool {
+        /// Check if this coordinate is orthogonally adjacent to another one.
+        fn is_orthogonal(self, other: Self) -> bool {
             let diffs = (
                 self.0.abs_diff(other.0),
                 self.1.abs_diff(other.1),
                 self.2.abs_diff(other.2),
             );
             diffs == (1, 0, 0) || diffs == (0, 1, 0) || diffs == (0, 0, 1)
+        }
+
+        /// Check if this coordinate is diagonally adjacent to another one.
+        fn is_diagonal(self, other: Self) -> bool {
+            let diffs = (
+                self.0.abs_diff(other.0),
+                self.1.abs_diff(other.1),
+                self.2.abs_diff(other.2),
+            );
+            diffs == (1, 1, 0) || diffs == (1, 0, 1) || diffs == (0, 1, 1) || diffs == (1, 1, 1)
         }
 
         /// Convert the coordinate to a GIFT coordinate.
@@ -116,10 +127,21 @@ mod effect {
                 self.2 as f32 * cell_width,
             )
         }
+
+        /// Calculate the Euclidean distance between this coordinate and another one.
+        fn distance(self, other: Self) -> NotNan<f32> {
+            let (dx, dy, dz) = (
+                self.0 as f32 - other.0 as f32,
+                self.1 as f32 - other.1 as f32,
+                self.2 as f32 - other.2 as f32,
+            );
+            NotNan::new(f32::sqrt(dx.mul_add(dx, dy.mul_add(dy, dz * dz))))
+                .expect_or_log("Euclidean distance of snake points should never be NaN")
+        }
     }
 
     /// A snake that can move through snake space.
-    #[derive(Clone, Debug, PartialEq)]
+    #[derive(Clone, PartialEq)]
     struct Snake {
         /// The lattice of points that the snake can move through.
         lattice: Lattice,
@@ -129,30 +151,91 @@ mod effect {
 
         /// The tail components of the snake.
         tail: VecDeque<Coord>,
+
+        /// The apple that the snake is moving towards.
+        apple: Coord,
+
+        /// The path the snake will take to the apple from its current position.
+        current_path: VecDeque<Coord>,
+    }
+
+    #[allow(
+        clippy::missing_fields_in_debug,
+        reason = "The lattice is unneeded noise"
+    )]
+    impl fmt::Debug for Snake {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            f.debug_struct("Snake")
+                .field("head", &self.head)
+                .field("tail", &self.tail)
+                .field("apple", &self.apple)
+                .field("current_path", &self.current_path)
+                .finish()
+        }
+    }
+
+    /// An error that can occur from the snake.
+    enum SnakeError {
+        /// We couldn't find a path from the head to the apple.
+        PathfindingFail,
+
+        /// We didn't have enough space to place the apple.
+        PlaceAppleFail,
     }
 
     impl Snake {
-        /// Create a new snake in the given lattice, placing the head at a random point.
+        /// Create a new snake in the given lattice, placing the head and apple at a random point,
+        /// but not calculating a path.
         fn new(lattice: Lattice, rng: &mut StdRng) -> Self {
             let head = lattice
                 .random_point(rng)
                 .expect_or_log("Should have at least one point in lattice");
 
+            let mut apple = head;
+            while apple == head {
+                apple = lattice
+                    .random_point(rng)
+                    .expect_or_log("Should have at least one point in lattice");
+            }
+
             Self {
                 lattice,
                 head,
                 tail: VecDeque::new(),
+                apple,
+                current_path: VecDeque::new(),
+            }
+        }
+
+        /// Clear the tail, and reset the head and apple to random positions.
+        fn reset(&mut self, rng: &mut StdRng) {
+            self.tail.clear();
+            self.current_path.clear();
+
+            self.head = self
+                .lattice
+                .random_point(rng)
+                .expect_or_log("Should have at least one point in lattice");
+
+            self.apple = self.head;
+            while self.apple == self.head {
+                self.apple = self
+                    .lattice
+                    .random_point(rng)
+                    .expect_or_log("Should have at least one point in lattice");
             }
         }
 
         /// Move the head to the new position.
         fn move_head(&mut self, new_pos: Coord) {
             debug_assert!(
-                self.head.is_adjacent(new_pos),
+                self.head.is_orthogonal(new_pos) || self.head.is_diagonal(new_pos),
                 "Can only move head to adjacent position"
             );
 
-            self.tail.pop_back();
+            if new_pos != self.apple {
+                self.tail.pop_back();
+            }
 
             debug_assert!(
                 !self.tail.contains(&new_pos),
@@ -163,9 +246,55 @@ mod effect {
             self.head = new_pos;
         }
 
-        /// View the underlying lattice of points.
-        fn lattice(&self) -> &Lattice {
-            &self.lattice
+        /// Place the apple randomly in the lattice.
+        fn place_random_apple(&mut self, rng: &mut StdRng) -> Result<(), SnakeError> {
+            let mut apple = self.head;
+
+            // If the snake and apple make up more than 3/4 of the lattice, then tell
+            // [`self.advance`] to fail.
+            if self.tail.len() + 2 >= self.lattice.points.len() * 3 / 4 {
+                return Err(SnakeError::PlaceAppleFail);
+            }
+
+            while apple == self.head || self.tail.contains(&apple) {
+                apple = self
+                    .lattice
+                    .random_point(rng)
+                    .expect_or_log("Should have at least one point in lattice");
+            }
+
+            self.apple = apple;
+            Ok(())
+        }
+
+        /// Calculate and store the shortest path from the head to the apple, avoiding the tail.
+        fn calculate_shortest_path(&mut self, allow_diagonals: bool) {
+            self.tail.make_contiguous();
+            self.current_path = self
+                .lattice
+                .shortest_path(
+                    self.head,
+                    self.apple,
+                    self.tail.as_slices().0,
+                    allow_diagonals,
+                )
+                .map_or_else(VecDeque::new, |v| v.into());
+            self.current_path.pop_front();
+        }
+
+        /// Advance the snake one space towards the apple.
+        fn advance(&mut self, rng: &mut StdRng, allow_diagonals: bool) -> Result<(), SnakeError> {
+            if let Some(new_pos) = self.current_path.pop_front() {
+                self.move_head(new_pos);
+            } else {
+                self.place_random_apple(rng)?;
+                self.calculate_shortest_path(allow_diagonals);
+                if self.current_path.is_empty() {
+                    return Err(SnakeError::PathfindingFail);
+                }
+            }
+
+            Ok(())
         }
     }
 
@@ -207,11 +336,7 @@ mod effect {
                         let dy = py - ly;
                         let dz = pz - lz;
 
-                        #[allow(
-                            clippy::suboptimal_flops,
-                            reason = "This format offers better clarity"
-                        )]
-                        let dist = f32::sqrt(dx * dx + dy * dy + dz * dz);
+                        let dist = f32::sqrt(dx.mul_add(dx, dy.mul_add(dy, dz * dz)));
 
                         dist <= cell_width / 2.0
                     })
@@ -238,9 +363,72 @@ mod effect {
             let idx = rng.gen_range(0..self.points.len());
             self.points.get(idx).copied()
         }
+
+        /// Find the shortest path between two points on the lattice
+        fn shortest_path(
+            &self,
+            start: Coord,
+            end: Coord,
+            avoid: &[Coord],
+            allow_diagonals: bool,
+        ) -> Option<Vec<Coord>> {
+            debug_assert!(
+                self.points.contains(&start),
+                "Start point must be on lattice"
+            );
+            debug_assert!(self.points.contains(&end), "End point must be on lattice");
+
+            let path = pathfinding::directed::dijkstra::dijkstra(
+                &start,
+                |&Coord(x, y, z)| {
+                    [
+                        Coord(x + 1, y, z),
+                        Coord(x - 1, y, z),
+                        Coord(x, y + 1, z),
+                        Coord(x, y - 1, z),
+                        Coord(x, y, z + 1),
+                        Coord(x, y, z - 1),
+                    ]
+                    .into_iter()
+                    .chain(if allow_diagonals {
+                        vec![
+                            Coord(x + 1, y + 1, z),
+                            Coord(x + 1, y, z + 1),
+                            Coord(x, y + 1, z + 1),
+                            Coord(x + 1, y - 1, z),
+                            Coord(x + 1, y, z - 1),
+                            Coord(x, y + 1, z - 1),
+                            Coord(x - 1, y + 1, z),
+                            Coord(x - 1, y, z + 1),
+                            Coord(x, y - 1, z + 1),
+                            Coord(x - 1, y - 1, z),
+                            Coord(x - 1, y, z - 1),
+                            Coord(x, y - 1, z - 1),
+                            Coord(x + 1, y + 1, z + 1),
+                            Coord(x + 1, y + 1, z - 1),
+                            Coord(x + 1, y - 1, z + 1),
+                            Coord(x + 1, y - 1, z - 1),
+                            Coord(x - 1, y + 1, z + 1),
+                            Coord(x - 1, y + 1, z - 1),
+                            Coord(x - 1, y - 1, z + 1),
+                            Coord(x - 1, y - 1, z - 1),
+                        ]
+                        .into_iter()
+                    } else {
+                        vec![].into_iter()
+                    })
+                    .filter(|coord| self.points.contains(coord) && !avoid.contains(coord))
+                    .map(move |coord| (coord, Coord(x, y, z).distance(coord)))
+                },
+                |pos| pos == &end,
+            )
+            .map(|(path, _dist)| path);
+            trace!(?start, ?end, ?path);
+            path
+        }
     }
 
-    /// Light up each light individually, one-by-one.
+    /// Create an AI snake that moves through the tree to collect the apple.
     #[derive(Clone, Debug, PartialEq, BaseEffect)]
     pub struct AiSnake {
         /// The config for this effect.
@@ -265,26 +453,40 @@ mod effect {
             clippy::semicolon_if_nothing_returned,
             reason = "this is a bodge for #[end_loop_in_test_or_bench]"
         )]
-        async fn run(self, driver: &mut dyn Driver) {
-            let radius = 1.2 / self.config.lattice_points_across_diameter as f32;
+        async fn run(mut self, driver: &mut dyn Driver) {
+            let mut fail_count: u8 = 0;
 
             #[end_loop_in_test_or_bench]
             loop {
-                for point in self.snake.lattice.iter_gift_coords() {
-                    driver.display_frame(FrameType::Frame3D(Frame3D::new(
-                        vec![FrameObject {
-                            object: Object::Sphere {
-                                center: point.into(),
-                                radius,
-                            },
-                            colour: self.config.head_colour,
-                            fadeoff: 0.05,
-                        }],
-                        false,
-                    )));
+                /// Reset the snake and `fail_count`, pause, and restart the loop.
+                macro_rules! reset_snake {
+                    () => {
+                        fail_count = 0;
+                        self.snake.reset(&mut self.rng);
+                        sleep!(Duration::from_millis(self.config.milliseconds_per_step));
+                        continue;
+                    };
+                }
 
-                    sleep!(Duration::from_millis(self.config.milliseconds_per_step));
-                    driver.clear();
+                match self
+                    .snake
+                    .advance(&mut self.rng, self.config.allow_diagonal_movement)
+                {
+                    Ok(()) => {
+                        fail_count = 0;
+                        sleep!(Duration::from_millis(self.config.milliseconds_per_step));
+                    }
+                    Err(SnakeError::PathfindingFail) => {
+                        fail_count += 1;
+                        debug!(?fail_count, "Pathfinding fail");
+                        if fail_count == 10 {
+                            reset_snake!();
+                        }
+                    }
+                    Err(SnakeError::PlaceAppleFail) => {
+                        debug!("Place apple fail");
+                        reset_snake!();
+                    }
                 }
             }
         }
