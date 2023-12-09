134142adc7ab709ab761e352342115b99f858fd8
DoctorDalek1963 <dyson.dyson@icloud.com>
2023-12-09 21:06:18 +0000

    Support dual clients

--- scanner/client/src/app.rs
+++ scanner/client/src/app.rs
@@ -2,6 +2,7 @@
 
 use crate::{camera::CameraWidget, controller::ControllerWidget};
 use eframe::egui::Context;
+use egui::Vec2;
 use std::sync::{Arc, RwLock};
 use tracing::{instrument, warn};
 use tracing_unwrap::ResultExt;
@@ -28,33 +29,17 @@ impl Default for AppState {
     }
 }
 
-/// The state of the client widgets.
-#[derive(Debug)]
-struct ClientWidgetsState {
-    /// A widget for the camera client.
-    camera_widget: Option<CameraWidget>,
-
-    /// A widget for the controller client.
-    controller_widget: Option<ControllerWidget>,
-}
-
-impl Default for ClientWidgetsState {
-    fn default() -> Self {
-        Self {
-            camera_widget: None,
-            controller_widget: None,
-        }
-    }
-}
-
 /// The app type itself.
-#[derive(Clone, Debug)]
+#[derive(Debug)]
 pub struct App {
     /// The state of the app.
     app_state: Arc<RwLock<AppState>>,
 
-    /// The state of the client widgets.
-    client_state: Arc<RwLock<ClientWidgetsState>>,
+    /// Do we have a camera widget?
+    camera_widget: Option<CameraWidget>,
+
+    /// Do we have a controller widget?
+    controller_widget: Option<ControllerWidget>,
 
     /// An async runtime used to send async messages.
     async_runtime: prokio::Runtime,
@@ -64,11 +49,11 @@ impl App {
     /// Create a new [`App`].
     pub fn new(_cc: &eframe::CreationContext) -> Self {
         let app_state = Arc::new(RwLock::new(AppState::default()));
-        let client_state = Arc::new(RwLock::new(ClientWidgetsState::default()));
         let async_runtime = prokio::Runtime::default();
         Self {
             app_state,
-            client_state,
+            camera_widget: None,
+            controller_widget: None,
             async_runtime,
         }
     }
@@ -77,26 +62,31 @@ impl App {
     #[instrument(skip_all)]
     fn display_gui_waiting_for_choice(&mut self, ctx: &Context) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.centered_and_justified(|ui| ui.vertical(|ui| {
-                ui.label("Please choose what type of client this device should be:");
+            ui.centered_and_justified(|ui| {
+                ui.vertical(|ui| {
+                    ui.label("Please choose what type of client this device should be:");
 
-                if ui.button("Camera").clicked() {
-                    self.client_state.write().unwrap_or_log().camera_widget = Some(CameraWidget::new(self.async_runtime.clone(), ctx));
-                    *self.app_state.write().unwrap_or_log() = AppState::WaitingForConnection;
-                }
+                    if ui.button("Camera").clicked() {
+                        self.camera_widget =
+                            Some(CameraWidget::new(self.async_runtime.clone(), ctx));
+                        *self.app_state.write().unwrap_or_log() = AppState::WaitingForConnection;
+                    }
 
-                if ui.button("Controller").clicked() {
-                    self.client_state.write().unwrap_or_log().controller_widget = Some(ControllerWidget::new(self.async_runtime.clone()));
-                    *self.app_state.write().unwrap_or_log() = AppState::WaitingForConnection;
-                }
+                    if ui.button("Controller").clicked() {
+                        self.controller_widget =
+                            Some(ControllerWidget::new(self.async_runtime.clone()));
+                        *self.app_state.write().unwrap_or_log() = AppState::WaitingForConnection;
+                    }
 
-                if ui.button("Both").clicked() {
-                    warn!("Clients with both camera and controller are not currently implemented properly");
-                    self.client_state.write().unwrap_or_log().camera_widget = Some(CameraWidget::new(self.async_runtime.clone(), ctx));
-                    self.client_state.write().unwrap_or_log().controller_widget = Some(ControllerWidget::new(self.async_runtime.clone()));
-                    *self.app_state.write().unwrap_or_log() = AppState::WaitingForConnection;
-                }
-            }));
+                    if ui.button("Both").clicked() {
+                        self.camera_widget =
+                            Some(CameraWidget::new(self.async_runtime.clone(), ctx));
+                        self.controller_widget =
+                            Some(ControllerWidget::new(self.async_runtime.clone()));
+                        *self.app_state.write().unwrap_or_log() = AppState::WaitingForConnection;
+                    }
+                })
+            });
         });
     }
 
@@ -119,26 +109,34 @@ impl App {
     /// Display the GUI for when we're connected to the server.
     #[instrument(skip_all)]
     fn display_gui_connected(&mut self, ctx: &Context) {
-        let mut client_state = self.client_state.write().unwrap_or_log();
-
-        if let Some(ref mut _camera) = client_state.camera_widget
-            && let Some(ref mut _controller) = client_state.controller_widget
+        if let Some(ref mut camera) = self.camera_widget
+            && let Some(ref mut controller) = self.controller_widget
         {
-            unimplemented!("Using both client types not yet implemented");
-        } else if let Some(ref mut camera) = client_state.camera_widget {
-            camera.respond_to_server_messages();
+            egui::CentralPanel::default().show(ctx, |ui| {
+                ui.vertical(|ui| {
+                    // The camera is only allowed to fill 75% of the vertical space. Without this
+                    // resriction, it would try to fill as much space as possible and push the
+                    // controller off the bottom of the window
+                    ui.allocate_ui(ui.max_rect().size() * Vec2::new(1.0, 0.75), |ui| {
+                        ui.add(camera)
+                    });
+
+                    ui.separator();
+                    ui.add(controller);
+                });
+            });
+        } else if let Some(ref mut camera) = self.camera_widget {
             egui::CentralPanel::default().show(ctx, |ui| {
                 ui.centered_and_justified(|ui| ui.add(camera));
             });
-        } else if let Some(ref mut controller) = client_state.controller_widget {
-            controller.respond_to_server_messages();
+        } else if let Some(ref mut controller) = self.controller_widget {
             egui::CentralPanel::default().show(ctx, |ui| {
                 ui.centered_and_justified(|ui| ui.add(controller));
             });
         } else {
             egui::CentralPanel::default().show(ctx, |ui| {
                 ui.centered_and_justified(|ui| {
-                    ui.label("ERROR: Connected to the server but no client initialised")
+                    ui.heading("ERROR: Connected to the server but no client initialised")
                 });
             });
         }
@@ -146,15 +144,13 @@ impl App {
 
     /// Respond to the server messages in the client widgets.
     fn respond_to_server_messages_in_client_widgets(&mut self) {
-        let mut client_state = self.client_state.write().unwrap_or_log();
-
-        if let Some(ref mut camera) = client_state.camera_widget {
+        if let Some(ref mut camera) = self.camera_widget {
             if let Some(new_state) = camera.respond_to_server_messages() {
                 *self.app_state.write().unwrap_or_log() = new_state;
             }
         }
 
-        if let Some(ref mut controller) = client_state.controller_widget {
+        if let Some(ref mut controller) = self.controller_widget {
             if let Some(new_state) = controller.respond_to_server_messages() {
                 *self.app_state.write().unwrap_or_log() = new_state;
             }
