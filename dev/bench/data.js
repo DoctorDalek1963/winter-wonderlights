cd0b4adf3cfd9923a2cc6853abaaab8f8d7095d2
github-action-benchmark <github@users.noreply.github.com>
2024-02-01 22:22:33 +0000

    add Benchmark (cargo) benchmark result for 657045c4d4e2eec06152fa6244fdbd9b3290f61b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706817920616,
+  "lastUpdate": 1706826153893,
   "repoUrl": "https://github.com/DoctorDalek1963/winter-wonderlights",
   "entries": {
     "Benchmark": [
@@ -4823,6 +4823,102 @@ window.BENCHMARK_DATA = {
             "name": "(ConvertFrameDriver) AiSnake",
             "value": 2386,
             "range": "± 1701",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dyson.dyson@icloud.com",
+            "name": "DoctorDalek1963",
+            "username": "DoctorDalek1963"
+          },
+          "committer": {
+            "email": "dyson.dyson@icloud.com",
+            "name": "DoctorDalek1963",
+            "username": "DoctorDalek1963"
+          },
+          "distinct": true,
+          "id": "657045c4d4e2eec06152fa6244fdbd9b3290f61b",
+          "message": "Use `env!()` macro for `{,SCANNER_}SERVER_URL` environment variables for WASM-based clients",
+          "timestamp": "2024-02-01T22:18:31Z",
+          "tree_id": "ea14c4af37eb60eb48d72aa38af6154d20c663ea",
+          "url": "https://github.com/DoctorDalek1963/winter-wonderlights/commit/657045c4d4e2eec06152fa6244fdbd9b3290f61b"
+        },
+        "date": 1706826153526,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "(SimpleDriver) DebugOneByOne",
+            "value": 4,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "(ConvertFrameDriver) DebugOneByOne",
+            "value": 4,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "(SimpleDriver) DebugBinaryIndex",
+            "value": 4,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "(ConvertFrameDriver) DebugBinaryIndex",
+            "value": 4,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "(SimpleDriver) MovingPlane",
+            "value": 1302,
+            "range": "± 267",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "(ConvertFrameDriver) MovingPlane",
+            "value": 1303,
+            "range": "± 274",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "(SimpleDriver) SplitPlane",
+            "value": 6216,
+            "range": "± 8",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "(ConvertFrameDriver) SplitPlane",
+            "value": 344116,
+            "range": "± 1521",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "(SimpleDriver) LavaLamp",
+            "value": 37593,
+            "range": "± 163",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "(ConvertFrameDriver) LavaLamp",
+            "value": 2398647,
+            "range": "± 18323",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "(SimpleDriver) AiSnake",
+            "value": 2224,
+            "range": "± 629",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "(ConvertFrameDriver) AiSnake",
+            "value": 2397,
+            "range": "± 1668",
             "unit": "ns/iter"
           }
         ]
