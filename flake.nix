835770ad77670dedfbf5dff773e9145acac5e647
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-07-26 23:10:43 +0100

    Add wasm32-unknown-unknown target to Rust toolchain in devShell

--- flake.nix
+++ flake.nix
@@ -216,6 +216,7 @@
                 (buildRustToolchain (toolchain:
                   toolchain.default.override {
                     extensions = ["rust-analyzer" "rust-src" "rust-std"];
+                    targets = ["wasm32-unknown-unknown"];
                   }))
                 pkgs.cargo-insta
                 pkgs.cargo-nextest
